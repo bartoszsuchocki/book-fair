1035271aaf03a67a806f7f3cbdd669f651910a27
Bartosz Suchocki <suchar1270@wp.pl>
2018-09-02 18:40:54 +0200

    Dodanie wyświetlania zakładki "o serwisie"

--- src/main/java/com/suchocki/bookfair/controller/MainController.java
+++ src/main/java/com/suchocki/bookfair/controller/MainController.java
@@ -1,15 +1,20 @@
 package com.suchocki.bookfair.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
 
-	@RequestMapping("/menu")
+	@GetMapping("/menu")
 	public String showEntryPage() {
 
 		return "mainmenu";
+	}
+
+	@GetMapping("/info")
+	public String displayInfoPage() {
+		return "info";
 	}
 
 }
