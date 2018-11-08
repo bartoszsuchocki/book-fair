2d4252ae31b0325a8ce1131565389b3fba8c53bc
Bartek Suchocki <suchar1270@wp.pl>
2018-11-08 15:34:07 +0100

    Poprawienie kodowania atrybutów rejestrowanych użytkowników

--- src/main/java/com/suchocki/bookfair/config/SecurityWebAppInitializer.java
+++ src/main/java/com/suchocki/bookfair/config/SecurityWebAppInitializer.java
@@ -1,7 +1,30 @@
 package com.suchocki.bookfair.config;
 
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+import org.springframework.web.multipart.support.MultipartFilter;
+
+import com.suchocki.bookfair.filter.EncodingFilter;
 
 public class SecurityWebAppInitializer extends AbstractSecurityWebApplicationInitializer {
-
+	
+	@Override
+	protected void beforeSpringSecurityFilterChain(ServletContext servletContext) {
+		insertFilters(servletContext, new EncodingFilter());
+	}
+	class MyFilter extends MultipartFilter{
+		@Override
+		protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+				FilterChain filterChain) throws ServletException, IOException {
+			super.doFilterInternal(request, response, filterChain);
+		}
+	}
+	
 }
